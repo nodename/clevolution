86822dd1d395497e7a010f4520ebd28b61fffe3f
Alan Shaw <alan@nodename.com>
2012-03-11 23:41:38 -0700

    testing with input images

--- src/clevolution.clj
+++ src/clevolution.clj
@@ -12,12 +12,13 @@
         [com.nodename.evolution.image_ops.binary :only [+ - and or xor min max mod]] :reload-all))
 
 (def image-file-name "F:\\generated-images\\test.png")
-(def input-files (list "F:\\generated-images\\z-movie\\0000.png" "F:\\generated-images\\z-movie\\0453.png"))
+; (def input-files (list "F:\\generated-images\\z-movie\\0000.png" "F:\\generated-images\\z-movie\\0453.png"))
+(def input-files (list "F:\\generated-images\\z-movie\\0000.png" "C:\\Users\\alan\\Pictures\\NodenameImages\\zhengred400.png"))
 
 ;(def max-depth 3)
-(def max-depth 3)
+(def max-depth 2)
 
-;;(generate-random-image-file image-file-name max-depth input-files)
-(generate-random-image-file image-file-name max-depth)
+(generate-random-image-file image-file-name max-depth input-files)
+;; (generate-random-image-file image-file-name max-depth)
 
 ;; face: (or (sin (cos (bw-noise 735 9 0.37795912888484595 200 200))) (cos (sin (bw-noise 83 7 0.18731404903090182 200 200))))
