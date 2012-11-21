60fa4436f05017251387c89491563332a51f466a
Alan Shaw <alan@nodename.com>
2012-11-20 20:50:49 -0800

    added lein-marginalia to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :source-path "src"
   :java-source-path "src"
   :dependencies [[org.clojure/clojure "1.3.0"]
-  		[org.clojars.serabe/rinzelight "0.0.3"]])
+  		[org.clojars.serabe/rinzelight "0.0.3"]]
+  ; lein 1:
+  :dev-dependencies [[lein-marginalia "0.7.1"]]
+  ; lein 2.0 dev-dependencies equivalent for plugins
+  :plugins [[lein-marginalia "0.7.1"]]
+  
+  )
