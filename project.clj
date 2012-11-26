7ecdc559877606730b3af9ade1a6b4f683828ee7
Alan Shaw <alan@nodename.com>
2012-11-26 12:46:38 -0800

    bumped version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject clevolution "0.1.0"
+(defproject org.clojars.nodename/clevolution "0.1.1"
   :description "Evolutionary art library in Clojure"
   :source-path "src"
-  :java-source-path "src"
+  :java-source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.3.0"]
-  		[org.clojars.serabe/rinzelight "0.0.3"]]
+  		[org.clojars.nodename/rinzelight "0.0.3"]
+    ]
   ; lein 1:
-  :dev-dependencies [[lein-marginalia "0.7.1"]]
+  ;:dev-dependencies [[lein-marginalia "0.7.1"]]
   ; lein 2.0 dev-dependencies equivalent for plugins
   :plugins [[lein-marginalia "0.7.1"]]
   
