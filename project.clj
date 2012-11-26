c6e45c054c3c5e8202580018d522d0b61b0487a2
Alan Shaw <alan@nodename.com>
2012-11-26 13:53:09 -0800

    removed lein 1 dev-dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,5 @@
   :source-path "src"
   :java-source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.3.0"]
-  		[org.clojars.nodename/rinzelight "0.0.3"]
-    ]
-  ; lein 1:
-  ;:dev-dependencies [[lein-marginalia "0.7.1"]]
-  ; lein 2.0 dev-dependencies equivalent for plugins
-  :plugins [[lein-marginalia "0.7.1"]]
-  
-  )
+                 [org.clojars.nodename/rinzelight "0.0.3"]]
+  :plugins [[lein-marginalia "0.7.1"]])
