588c835369312d8352a703bc43b16111ced8e2e3
Alan Shaw <alan@nodename.com>
2012-12-10 19:50:09 -0800

    added clevolution-version to image file headers

--- src/clevolution/file_io.clj
+++ src/clevolution/file_io.clj
@@ -11,6 +11,9 @@
 ;; for upper- and lower-case letters in the four-letter chunk name:
 ;; http://en.wikipedia.org/wiki/Portable_Network_Graphics#.22Chunks.22_within_the_file
 (def generator-chunk-name "gnTr")
+(def clevolution-version-chunk-name "clVn")
+(def clevolution-version "0.1.1")
+(def first-named-version "0.1.1")
 
 (defn get-png-imagewriter
 	"Return an ImageWriter for PNG images"
@@ -22,11 +25,13 @@
 
 
 (defn make-generator-metadata
-	"Create a PNGMetadata containing generator-string in its generator header chunk"
+	"Create a PNGMetadata containing generator-string and clevolution-version"
 	[generator-string]
 	(let [png-metadata (PNGMetadata.)]
 	(.add (.unknownChunkType png-metadata) generator-chunk-name)
 	(.add (.unknownChunkData png-metadata) (.getBytes generator-string))
+	(.add (.unknownChunkType png-metadata) clevolution-version-chunk-name)
+	(.add (.unknownChunkData png-metadata) (.getBytes clevolution-version))
 	png-metadata))
 
 
@@ -59,23 +64,23 @@
     (.dispose imagereader)
     bi))
 
-(defmulti get-generator-string class)
+(defmulti get-header-string (fn [source _] (class source)))
 
-(defmethod get-generator-string PNGMetadata
-	[png-metadata]
+(defmethod get-header-string PNGMetadata
+	[png-metadata chunk-name]
 	(let [dataArrayList (.unknownChunkData png-metadata)
 	      typeArrayList (.unknownChunkType png-metadata)]
 	(loop [i 0]
 		(cond
 			(>= i (.size dataArrayList))
 				""
-			(= (.get typeArrayList i) generator-chunk-name)
+			(= (.get typeArrayList i) chunk-name)
 				(String. (.get dataArrayList i))
 			:else
 				(recur (inc i))))))
 
-(defmethod get-generator-string String
-  [uri]
+(defmethod get-header-string String
+  [uri chunk-name]
   (let [input-stream (ImageIO/createImageInputStream (File. uri))
         imagereader (get-imagereader input-stream)
         _ (.setInput imagereader input-stream true)
@@ -83,7 +88,19 @@
         input-metadata (.getImageMetadata imagereader image-index)
         _ (.close input-stream)
         _ (.dispose imagereader)]
-    (get-generator-string input-metadata)))
+    (get-header-string input-metadata chunk-name)))
+
+
+(defn get-generator-string
+  [source]
+  (get-header-string source generator-chunk-name))
+
+(defn get-clevolution-version
+  [source]
+  (let [version (get-header-string source clevolution-version-chunk-name)]
+    (if (= "" version)
+      first-named-version
+      version)))
 
   
 (defn save-image
