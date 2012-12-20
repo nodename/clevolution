294dc923b8a70df4dae604f2fc12db677fc57d0c
Alan Shaw <alan@nodename.com>
2012-12-19 19:06:18 -0800

    added clisk dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.clojars.nodename/clevolution "0.1.1"
+(defproject com.nodename/clevolution "0.1.1"
   :description "Evolutionary art library in Clojure"
   :source-paths ["src"]
   :java-source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojars.nodename/rinzelight "0.0.3"]]
+                 [org.clojars.nodename/rinzelight "0.0.3"]
+                 [net.mikera/clisk "0.4.3"]]
   :plugins [[lein-marginalia "0.7.1"]])
