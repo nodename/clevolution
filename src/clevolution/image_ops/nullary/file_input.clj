5e6ca79a422e801b80b8e3b78d3843c11ce8f9ed
Alan Shaw <alan@nodename.com>
2012-12-19 16:47:43 -0800

    moved get-imagereader and read-iage-from-file to new namespace file_input.clj

---
+++ src/clevolution/image_ops/nullary/file_input.clj
@@ -0,0 +1,21 @@
+(ns clevolution.image-ops.nullary.file-input
+  (:import (java.io File)
+           (javax.imageio ImageIO)))
+
+(defn get-imagereader
+	[inputstream]
+	(let [iterator (ImageIO/getImageReaders inputstream)]
+	(if-not (.hasNext iterator) 
+		(throw (Exception. "No image reader found for stream")))
+	(.next iterator)))
+
+(defn read-image-from-file
+  [uri]
+  (let [input-stream (ImageIO/createImageInputStream (File. uri))
+        imagereader (get-imagereader input-stream)
+        _ (.setInput imagereader input-stream true)
+        image-index 0
+        bi (.read imagereader image-index)]
+    (.close input-stream)
+    (.dispose imagereader)
+    bi))
