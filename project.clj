b7b9e02da6756dd3cc23482cda5bc9609691762b
Alan Shaw <alan@nodename.com>
2013-03-26 22:47:08 -0700

    Updated dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Evolutionary art library in Clojure"
   :source-paths ["src"]
   :java-source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.nodename/rinzelight "0.0.3"]
-                 [net.mikera/clisk "0.4.3"]]
+                 [net.mikera/clisk "0.7.0"]]
   :plugins [[lein-marginalia "0.7.1"]])
