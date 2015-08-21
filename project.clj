b8dc420463d3a365a6fd7c1b85171179b3cef5f6
Alan Shaw <alan@nodename.com>
2015-08-20 20:14:27 -0700

    Not using local copy of clisk

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.nodename/clevolution "0.1.1"
   :description "Evolutionary art library in Clojure"
   :source-paths ["src/clj" #_"../clisk/src/main/clojure" #_"../seesaw/src"]
-  :java-source-paths ["src/java" "../clisk/src/main/java"]
+  :java-source-paths ["src/java" #_"../clisk/src/main/java"]
   :dependencies [[org.clojure/clojure "1.8.0-alpha4"]
                  [net.mikera/clisk "0.10.1"]
                  [net.mikera/vectorz-clj "0.31.0"]
