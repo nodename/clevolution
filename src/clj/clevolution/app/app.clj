50677ce66296397ea40ea5dbb44a215545957739
Alan Shaw <alan@nodename.com>
2015-10-27 21:29:51 -0700

    added Clevolution menu and preferences sialog

--- src/clj/clevolution/app/app.clj
+++ src/clj/clevolution/app/app.clj
@@ -1,5 +1,11 @@
 (ns clevolution.app.app
-  (:require [seesaw.core :as seesaw]
+  (:require [seesaw.core :as seesaw
+             :refer [action return-from-dialog value to-frame alert
+                     custom-dialog label combobox slider flow-panel
+                     pack! show!]]
+            [seesaw.mig :refer [mig-panel]]
+            [seesaw.border :refer [line-border]]
+            [seesaw.font :refer [font default-font]]
             [clevolution.imagedata :refer [merge-view-elements do-calc]]
             [clevolution.file-output :refer [get-generator make-generator-metadata write-image-to-file]]
 
@@ -144,6 +150,70 @@
 
 
 
+(defn open-preferences-dialog
+  []
+  (let [ok-act (action
+                 :name "Ok"
+                 :handler (fn [e] (return-from-dialog e (value (to-frame e)))))
+        cancel-act (action :name "Cancel"
+                           :handler (fn [e] (return-from-dialog e nil)))]
+    (-> (custom-dialog
+          :title  "Preferences"
+          :modal? true
+          :resizable? false
+          :content (mig-panel
+                     :border (line-border)
+                     :items [[(label :font (font :from (default-font "Label.font") :style :bold)
+                                     :text "Clevolution options")
+                              "gaptop 10, wrap"]
+
+                             [:separator "growx, wrap, gaptop 10, spanx 2"]
+
+                             ["Number of Mutations"]
+
+                             [(slider :id :num-mutations
+                                      :min 0 :max 20 :value (:num-mutations @app-state)
+                                      :minor-tick-spacing 1 :major-tick-spacing 10
+                                      :paint-labels? true)
+                              "wrap"]
+
+                             [(flow-panel :align :right :items [ok-act cancel-act])
+                              "spanx 2" "alignx right"]]))
+        pack!
+        show!)))
+
+
+(defn make-clevolution-menu
+  []
+  (let [preferences-menu-item (seesaw/menu-item
+                                :text "Preferences..."
+                                :listen [:action (fn [_]
+                                                   (let [value (open-preferences-dialog)]
+                                                     (swap! app-state
+                                                            merge value)))])]
+
+    (doto (JMenu. "Clevolution")
+      (.add preferences-menu-item))))
+
+
+(defn make-file-menu
+  [frame]
+  (let [^JMenuItem load-menuitem (seesaw/menu-item
+                                   :text "Load..."
+                                   :listen [:action (fn [_] (load-file-dialog frame))])
+        ^JMenuItem save-menuitem (seesaw/menu-item
+                                   :text "Save As..."
+                                   :listen [:action (fn [_] (save-file-dialog frame))])
+        ^JMenuItem save-history-menu-item (seesaw/menu-item
+                                            :text "Save History As..."
+                                            :listen [:action (fn [_] (save-history-dialog frame))])]
+
+    (doto (JMenu. "File")
+      (.add load-menuitem)
+      (.add save-menuitem)
+      (.add save-history-menu-item))))
+
+
 (defn create-app-frame
   [image generator context title]
   (image-timetravel/forget-everything!)
@@ -159,22 +229,11 @@
     (appstate/initialize-state! content-panel)
     (currentimagestate/initialize-state! generator image context)
 
-    (let [^JMenuItem load-menuitem (seesaw/menu-item
-                                     :text "Load..."
-                                     :listen [:action (fn [_] (load-file-dialog frame))])
-          ^JMenuItem save-menuitem (seesaw/menu-item
-                                     :text "Save As..."
-                                     :listen [:action (fn [_] (save-file-dialog frame))])
-          ^JMenuItem save-history-menu-item (seesaw/menu-item
-                                              :text "Save History As..."
-                                              :listen [:action (fn [_] (save-history-dialog frame))])
-
-          file-menu (doto (JMenu. "File")
-                      (.add load-menuitem)
-                      (.add save-menuitem)
-                      (.add save-history-menu-item))
+    (let [clevolution-menu (make-clevolution-menu)
+          file-menu (make-file-menu frame)
 
           menu-bar (doto (JMenuBar.)
+                     (.add clevolution-menu)
                      (.add file-menu))]
 
       (doto frame
