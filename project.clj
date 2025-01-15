9890aa683602e71bc454d5710cff796c917b8f94
Alan Shaw <alan@nodename.com>
2025-01-14 19:11:17 -0800

    Updated dependencies

--- project.clj
+++ project.clj
@@ -4,20 +4,20 @@
   :java-source-paths ["src/java" #_"../clisk/src/main/java"]
   :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [[org.clojure/clojure "1.8.0-alpha5"]
-                 [net.mikera/clisk "0.10.1"]
-                 [net.mikera/vectorz-clj "0.31.0"]
-                 [net.mikera/imagez "0.6.0"]
+                 [net.mikera/clisk "0.11.0"]
+                 [net.mikera/vectorz-clj "0.48.0"]
+                 [net.mikera/imagez "0.12.0"]
                  [net.mikera/mikera-gui "0.3.1"]
-                 [instaparse "1.4.1"]
+                 [instaparse "1.5.0"]
                  [net.mikera/telegenic "0.0.1"]
                  #_[jmagick/jmagick "6.6.9"]
-                 [seesaw "1.4.5" :exclusions [org.clojure/clojure]]
+                 [seesaw "1.5.0" :exclusions [org.clojure/clojure]]
                  #_[com.miglayout/miglayout "3.7.4"]
                  #_[com.jgoodies/forms "1.2.1"]
                  #_[org.swinglabs.swingx/swingx-core "1.6.3"]
                  #_[j18n "1.0.2"]
                  #_[com.fifesoft/rsyntaxtextarea "2.5.6"]]
-  :plugins [[lein-marginalia "0.7.1"]]
+  :plugins [[lein-marginalia "0.9.2"]]
 
   :main clevolution.main
   )
